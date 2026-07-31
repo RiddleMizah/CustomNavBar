dc04a204df4a841c3a210c8951b794fe994f061d
Riddle <jake.blankenship@aol.com>
2026-07-30 21:58:14 -0400

    Add R2-D2 BLE central interface

---
+++ r2d2-standalone-flipper/overlay/applications_user/r2d2_controller/r2d2_ble.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <furi.h>
+#include <furi_ble/event_dispatcher.h>
+#include "r2d2_events.h"
+
+typedef struct {
+    FuriMessageQueue* queue;
+} R2Ble;
+
+void r2_ble_init(R2Ble* ble, FuriMessageQueue* queue);
+BleEventAckStatus r2_ble_event_handler(void* event, void* context);
+
+bool r2_ble_start_scan(void);
+bool r2_ble_stop_scan(void);
+bool r2_ble_connect(uint8_t address_type, const uint8_t address[6]);
+bool r2_ble_discover_service(uint16_t connection_handle);
+bool r2_ble_discover_write_characteristic(
+    uint16_t connection_handle,
+    uint16_t start_handle,
+    uint16_t end_handle);
+bool r2_ble_write(
+    uint16_t connection_handle,
+    uint16_t characteristic_handle,
+    const uint8_t* data,
+    uint8_t length);
+void r2_ble_disconnect(uint16_t connection_handle);
